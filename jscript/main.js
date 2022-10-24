11821ff81ed528b6ca5347c9ae807cf928c99034
Tappobill <christian.salvati@hotmail.it>
2022-10-24 16:16:57 +0200

    Quadrati, Stile e Darkmode

--- jscript/main.js
+++ jscript/main.js
@@ -1,29 +1,53 @@
+let container = document.querySelector(".container");
+console.log(container);
+
 for (let i = 1; i <= 100; i++) {
 
 
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
+        let variant = "fizzbuzz";
+        container.innerHTML += `<div class="box box--${variant}">${variant}</div>`;
+        console.log(variant)
     }
 
     else if (i % 5 === 0) {
-        console.log("Buzz")
+        let variant = "buzz";
+        container.innerHTML += `<div class="box box--${variant}">${variant}</div>`;
+        console.log(variant)
     }
 
     else if (i % 3 === 0) {
-        console.log("Fizz")
+        let variant = "fizz";
+        container.innerHTML += `<div class="box box--${variant}">${variant}</div>`;
+        console.log(variant)
     }
 
     else {
+        container.innerHTML += `<div class="box">${i}</div>`;
         console.log(i)
     }
 
-
-
-
-
-
-
-
-
-
 }
+
+var r = document.querySelector(":root");
+console.log(r);
+
+let moon = document.getElementById("dark-mode");
+
+let darkModeStatus = true;
+
+moon.addEventListener("click", function () {
+    if (darkModeStatus) {
+        r.style.setProperty("--background-body", "white");
+        r.style.setProperty("--body-text", "black");
+        moon.classList.remove("fa-moon");
+        moon.classList.add("fa-sun");
+        darkModeStatus = false;
+    } else {
+        r.style.setProperty("--background-body", "black");
+        r.style.setProperty("--body-text", "white");
+        moon.classList.remove("fa-sun");
+        moon.classList.add("fa-moon");
+        darkModeStatus = true;
+    }
+});
